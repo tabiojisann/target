4181dfd9464cb151c43b4ac1b27fd15cf0e21227
tabiojisann <takumiezumi@icloud.com>
2020-07-10 14:47:49 +0900

    up

--- resources/views/target/test.blade.php
+++ resources/views/target/test.blade.php
@@ -1,35 +1,27 @@
-<?php 
-
-class User {
-  public $name;
-
-  public function __construct($name){
-    $this->name = $name;
-  }
-
-  final public function sayHi(){
-    echo "Hello $this->name !";
-  }
-}
-
-class SpecialUser extends User {
-  public function sayHi(){
-    echo "I am Special $this->name";
-  }
-}
-
-$junya = new User("junya");
-$kono  = new SpecialUser("kono");
-
-$junya->sayHi();
-$kono->sayhi();
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>test</title>
+</head>
+<body class="body">
 
 
-  
+  <div class="box">
+    <div class="php">
 
-?>
-
-
-
-
-
+      <?php
+       $Pokemon = ['ヒトカゲ', 'ゼニガメ', 'フシギダネ'];
+         foreach( $Pokemon as $p)
+         echo $p . " ";
+         endforeach
+      ?>
+    </div>
+      <?php
+        echo $p;
+      ?>
+  </div> 
+</body>
+</html>
