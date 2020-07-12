8f9d82b97379611abc562b9b0a060b3d67d8bcb0
tabiojisann <takumiezumi@icloud.com>
2020-07-12 22:41:12 +0900

    fix

--- resources/views/target/test.blade.php
+++ resources/views/target/test.blade.php
@@ -51,6 +51,35 @@
   answer( $tall); 
 ?> --->
 
+<?php
+
+$records = array(
+  array(
+      'id' => 2135,
+      'first_name' => 'John',
+      'last_name' => 'Doe',
+  ),
+  array(
+      'id' => 3245,
+      'first_name' => 'Sally',
+      'last_name' => 'Smith',
+  ),
+  array(
+      'id' => 5342,
+      'first_name' => 'Jane',
+      'last_name' => 'Jones',
+  ),
+  array(
+      'id' => 5623,
+      'first_name' => 'Peter',
+      'last_name' => 'Doe',
+  )
+);
+
+$first_names = array_column($records, 'id');
+var_dump($first_names);
+
+?>
 
 <?php
   
@@ -218,6 +247,10 @@ $pokemon = [
   //   echo $name;
   //   echo '<br/>'; 
   // }
+
+  
+
+  
 
   // var_dump($pokemon);
 
